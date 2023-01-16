71320a261418a784a619a5411d1e3b4f41e1efe2
Stephen Axtell <axtell.stephen@gmail.com>
2023-01-16 19:18:37 +0200

    working with functions

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const age = 36;
 let message = "Hello World";
 alert(message);
-alert(age);
 
 let hobbies = ["reading", "hiking", "sports"];
 alert(hobbies);
 console.log(hobbies[2]);
 let job = { title: "Developer", place: "Cape Town", yearsService: 5 };
+console.log(job.title);
+
+let yearsAdult;
+
+function calcAdultYears(age) {
+  console.log(`In function with age: ${age}`);
+  return age - 18;
+}
+
+yearsAdult = calcAdultYears(45);
+console.log(yearsAdult);
+
+yearsAdult = calcAdultYears(55);
+console.log(yearsAdult);
