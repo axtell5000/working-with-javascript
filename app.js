b45ec499a2cf2ed0d3a4c7f55b7a4cdbb4de4abd
Stephen Axtell <axtell.stephen@gmail.com>
2023-01-16 10:50:31 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const age = 36;
+let message = "Hello World";
+alert(message);
+alert(age);
+
+let hobbies = ["reading", "hiking", "sports"];
+alert(hobbies);
+console.log(hobbies[2]);
+let job = { title: "Developer", place: "Cape Town", yearsService: 5 };
